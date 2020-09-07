26a83bfea63812f38649c4561ce5083809d94dab
Juan Alberto Solis Ruano <juansolisgtsk@hotmail.com>
2020-09-07 11:48:39 -0600

    Creacion de repositorio con el proyecto de java y un archivo txt de prueba

---
+++ lab1_JuanSolis_1136119/src/lab1_juansolis_1136119/Lab1_JuanSolis_1136119.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_juansolis_1136119;
+
+/**
+ *
+ * @author warfa
+ */
+public class Lab1_JuanSolis_1136119 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
